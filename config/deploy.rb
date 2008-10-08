03e95606e11956da09e752a35d09c9094727fcfe
Ken Gass <kgass@shay.com>
2008-10-08 09:43:57 -0500

    right user3

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repository,  "."
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/vhosts/amffunds.com/subdomains/docs/apps/#{application}"
+set :deploy_to, "/var/www/vhosts/amffunds.com/subdomains/docs/httpdocs/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
